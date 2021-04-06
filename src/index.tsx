ee19407cf813322e7097d6d466a9887f1edb0238
Albert Dias <albert.uft@gmail.com>
2021-04-06 12:54:27 -0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text> Eu amo vcs</Text>
+    </View>
+  );
+};
+
+export default App;
