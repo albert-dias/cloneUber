ca0d9e41a7389f8b5e07cd8f853db5cb9552f39d
Albert Dias <albert.uft@gmail.com>
2021-04-06 21:38:06 -0300

    installing react-native-maps

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import MapView from 'react-native-maps';
+import {View} from 'react-native';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text> Eu amo vcs</Text>
+    <View style={{flex: 1}}>
+      <MapView
+        style={{flex: 1}}
+        region={{
+          latitude: -5.855653225866471,
+          longitude: -35.26101974818065,
+          latitudeDelta: 0.01143,
+          longitudeDelta: 0.0134,
+        }}
+        showsUserLocation
+        loadingEnabled
+      />
     </View>
   );
 };
